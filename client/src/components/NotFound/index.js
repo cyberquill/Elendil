c1f8e825562a0c769f5304c730bd55fb4c5207b5
brij <brijgopalbharadwaj1999@gmail.com>
2019-05-04 17:22:08 +0530

    SERVER: DB refactoring, API calls revised

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Notfound() {
-    return <div className="display-1 text-dark">Not Found!!!</div>;
+    return (
+        <div className="display-1 text-dark text-center mt-5">Not Found!!!</div>
+    );
 }
