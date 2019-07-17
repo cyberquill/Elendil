0ef0f2d4f844aeb8ecfaf9df65b7a8bc49858bf8
brij <brijgopalbharadwaj1999@gmail.com>
2019-07-17 20:07:17 +0530

    FILE STRUCTURE REVAMP

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-
-const 
+const path = require('path'),
     passport = require('passport'),
     express = require('express'),
     bodyParser = require('body-parser'),
-
-{ users, instructors, students, courses, lectures, 
-    questions, answers } = require('./routes/api');
+    {
+        users,
+        instructors,
+        students,
+        courses,
+        lectures,
+        questions,
+        answers,
+    } = require('./routes/api');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 //==========================================================================
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,12 +23,16 @@ require('./config/passport')(passport);
 //==========================================================================
 // app.use("/instructor/dashboard", InstDashBRoutes);
 //==========================================================================
-app.use("/api/users", users);
-app.use("/api/instructors", instructors);
-app.use("/api/students", students);
-app.use("/api/courses", courses);
-app.use("/api/lectures", lectures);
-app.use("/api/questions", questions);
-app.use("/api/answers", answers);
+app.use('/api/users', users);
+app.use('/api/instructors', instructors);
+app.use('/api/students', students);
+app.use('/api/courses', courses);
+app.use('/api/lectures', lectures);
+app.use('/api/questions', questions);
+app.use('/api/answers', answers);
 //==========================================================================
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 app.listen(port, () => console.log(`Server Online on port ${port}...`));
